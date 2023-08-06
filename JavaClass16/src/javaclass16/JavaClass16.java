47850031a9e6eea7f1b63bbc288b1a15a6d4fce6
sabithassann <sabithassan006@gmail.com>
2023-08-06 12:58:26 +0600

    ADD
    class

---
+++ JavaClass16/src/javaclass16/JavaClass16.java
@@ -0,0 +1,15 @@
+
+package javaclass16;
+
+import method.JavaClass16Method;
+
+
+public class JavaClass16 {
+
+
+    public static void main(String[] args) {
+        JavaClass16Method.stic(); //no need to create object//static call
+        
+    }
+    
+}
---
+++ JavaClass16/src/method/JavaClass16Method.java
@@ -0,0 +1,16 @@
+
+package method;
+
+
+public class JavaClass16Method {
+    
+    
+    public static int value;
+    
+    public static void stic () {
+        System.out.println(3.1416);//static no need to call or create object 
+    }
+    
+}
+    
+
