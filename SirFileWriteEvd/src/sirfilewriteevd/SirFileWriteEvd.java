0655161676d898905234087dfdbd0fea608f934b
sabithassann <sabithassan006@gmail.com>
2023-09-10 12:50:53 +0600

    EvdEXam
    evdFileWrite

---
+++ SirFileWriteEvd/src/sirfilewriteevd/SirFileWriteEvd.java
@@ -0,0 +1,17 @@
+
+package sirfilewriteevd;
+
+import view.WriteFile;
+
+
+public class SirFileWriteEvd {
+
+
+    public static void main(String[] args) {
+        WriteFile f=new WriteFile();
+        
+        f.setLocationRelativeTo(null);//to set display in middle
+        f.setVisible(true);
+    }
+    
+}
