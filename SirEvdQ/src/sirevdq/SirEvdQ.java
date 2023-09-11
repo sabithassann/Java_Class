c7ba687dfe7e608f1f66fea281ecbbabc1d3abbd
sabithassann <sabithassan006@gmail.com>
2023-09-11 12:44:51 +0600

    evd
    evdupdate

---
+++ SirEvdQ/src/sirevdq/SirEvdQ.java
@@ -0,0 +1,17 @@
+
+package sirevdq;
+
+import view.Evd;
+
+
+public class SirEvdQ {
+
+
+    public static void main(String[] args) {
+        Evd show=new Evd();
+        show.setLocationRelativeTo(null);
+        show.setVisible(true);
+                
+    }
+    
+}
