89d29ce3b2646ce083def84bfbad60d70b181cea
sabithassann <sabithassan006@gmail.com>
2023-10-03 13:08:41 +0600

    tab
    tab

---
+++ Problem/SirEvdFourField/src/sirevdfourfield/SirEvdFourField.java
@@ -0,0 +1,16 @@
+
+package sirevdfourfield;
+
+import view.Design;
+
+
+public class SirEvdFourField {
+
+
+    public static void main(String[] args) {
+        Design show=new Design();
+        show.setVisible(true);
+        show.setLocationRelativeTo(null);
+    }
+    
+}
